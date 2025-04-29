befa773b1457490b60ea9ca97622922c041d92ed
wekkin-ubuntu <dengwq1997@163.com>
2025-04-29 13:21:31 +0800

    添加应用程序图标和相关配置

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(current_dir)
 
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QIcon
 from UI.UI_App_Root import MainWindow
 from utils.db_manager import DBManager
 from utils.logger import Logger
@@ -27,6 +28,15 @@ def main():
         # 启动应用
         logger.info("正在创建应用实例...")
         app = QApplication(sys.argv)
+        
+        # 设置应用图标
+        icon_path = os.path.join(current_dir, 'icons', 'icons8-abscissa-96.png')
+        if os.path.exists(icon_path):
+            app.setWindowIcon(QIcon(icon_path))
+            logger.info("已设置应用程序图标")
+        else:
+            logger.warning(f"图标文件不存在: {icon_path}")
+        
         window = MainWindow()
         window.show()
         logger.info("应用程序界面已显示")
